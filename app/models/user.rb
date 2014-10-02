d2edbe32f5ddb6303b0bb650725fcfd2c86a84bc
Tan Doan <tdoan87@gmail.com>
2014-10-02 14:29:07 -0600

    omniauth working with facebook

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+
+  def self.find_or_create_by_auth(auth_data)
+    user = self.find_or_create_by(provider: auth_data["provider"], uid: auth_data['uid'])
+    if user.name != auth_data['info']['name']
+      user.name = auth_data['info']['name']
+      user.first_name = auth_data['info']['first_name']
+      user.save
+    end
+    return user
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
 
+  get '/auth/facebook/callback', to: 'sessions#create'
+
+  get '/login' => redirect('/auth/facebook'), as: :login
+  get '/logout' => 'sessions#destroy', as: :logout
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
