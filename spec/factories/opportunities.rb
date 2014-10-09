1a0107560c5f639b3fde984d35880ae9251468df
Tan Doan <tdoan87@gmail.com>
2014-10-09 11:20:07 -0600

    class within test what the?? test green yaay

--- spec/factories/opportunities.rb
+++ spec/factories/opportunities.rb
@@ -1,6 +1,9 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :opportunity, :class => 'Opportunities' do
+  factory :opportunity do
+    organization_name 'Help The Kids'
+    title 'Kid Helper'
+    description 'Them kids be needing help'
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,7 +8,6 @@ require 'capybara/rspec'
 require 'capybara/rails'
 
 
-
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
